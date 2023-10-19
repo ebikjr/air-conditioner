86ff179340da6bd737d9091c11662546d52bb2da
ebikjr <ebikjrinc@gmail.com>
2023-10-19 10:35:53 +0700

    upd cpyrght

--- src/components/layouts/Copyright.tsx
+++ src/components/layouts/Copyright.tsx
@@ -4,41 +4,6 @@ import * as pkg from '~/../package.json'
 
 const socialList = [
   {
-    type: 'github',
-    color: 'inherit',
-    icon: 'i-mdi-github',
-    label: 'GitHub: YunYouJun',
-    href: 'https://github.com/YunYouJun',
-  },
-  {
-    type: 'telegram',
-    color: '#1da1f2',
-    icon: 'i-mdi-telegram',
-    label: 'Telegram Channel',
-    href: 'https://t.me/elpsycn',
-  },
-  {
-    type: 'weibo',
-    color: '#DB2828',
-    icon: 'i-mdi-sina-weibo',
-    label: '微博：机智的云游君',
-    href: 'http://weibo.com/jizhideyunyoujun',
-  },
-  {
-    type: 'twitter',
-    color: '#1da1f2',
-    icon: 'i-mdi-twitter',
-    label: 'Twitter: YunYouJun',
-    href: 'https://twitter.com/YunYouJun',
-  },
-  {
-    type: 'wechat',
-    color: '#1AAD19',
-    icon: 'i-mdi-wechat',
-    label: '微信公众号：云游君',
-    href: 'https://cdn.yunyoujun.cn/img/about/white-qrcode-and-search.jpg',
-  },
-  {
     type: 'blog',
     color: '#6435C9',
     icon: 'i-mdi-earth',
@@ -53,7 +18,7 @@ const Copyright: React.FC = () => {
       <div className="ac-text flex justify-center items-center m-2">
         {'© '}
         <a href={pkg.repository.url} target="_blank" rel="noreferrer">
-          Yun Air Conditioner
+          Air Conditioner Simulator Game
         </a>
         <a
           className="inline-flex"
@@ -68,7 +33,7 @@ const Copyright: React.FC = () => {
         </a>
       </div>
       <p>
-        {`2019 - ${new Date().getFullYear()}`}
+        {`2023 - ${new Date().getFullYear()}`}
       </p>
       <div className="text-center">
         {socialList.map(item => (
